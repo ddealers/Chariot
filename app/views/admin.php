e2beb7d01a401f29f713a48008386a17a4c835a4
Wenceslao Negrete <vampediem@gmail.com>
2014-06-17 00:03:12 -0500

    added routes

---
+++ app/views/admin.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	</head>
+	<body>
+		<a href="formulario.html">Renta diaria</a>
+		<a href="formulario2.html">Renta de autobúses</a>
+		<a href="formularioAca.html">Viaje Acapulco</a>
+		<a href="formularioVera.html">Viaje Tecolutla Veracruz </a>
+	</body>
+</html>
---
+++ app/views/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+	<html lang="es">
+	<head>
+		<title>Chariot rent-a-car</title>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+		<link href='http://fonts.googleapis.com/css?family=Advent+Pro:400,500' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Alegreya+Sans+SC:400,500' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<div class="centrar">
+		<nav class="menu">
+			<ul>
+				<li> <a href="index.html#servicios">SERVICIOS</a></li>
+				<li> <a href="index.html#ofertas">OFERTAS</a></li>
+				<li> <a href="contacto.html">CONTACTO</a></li>
+			</ul>
+		</nav>
+		<section class="home">
+			<img src="../assets/logo_chariot.png">
+			<p>Somos una empresa responsable, que brinbda distintos servicios de manera que disfrutarás tu estancia, "Porque nos gusta que visites nuestra ciudad"</p>
+			<a href="" class="boton1">RESERVA AHORA</a>
+		</section>
+		<section id="servicios" class="servicios"> 
+			<h1><span>Servicios</span></h1> 
+			<ul class="imagenes">
+				<li ><a href="categorias.html"><img src="../assets/renta_autos1.jpg"><span>Renta de Autos</span></a> </li>
+				<li ><a href="categorias.html"><img src="../assets/autobuses.jpg"><span>Renta de Autobúses</span></a> </li>
+				<li ><a href="categorias.html"><img src="../assets/piramide.jpg"><span>Chariot Tours</span></a> </li>
+				<li ><a href="categorias.html"><img src="../assets/magnicharter.jpg"><span>Magnicharters</span></a> </li>
+			</ul>
+		</section>
+    	<footer>
+    		<span>
+		    	<ul>
+                    
+					   <li>chariot rent-a-car</li>
+					   <li>@chariot rent-a-car</li>
+                   
+				</ul>
+			</span>
+            <cite><div>CUENTANOS TU EXPERENCIA A TRAVÉS DE LAS REDES SOCIALES.</div></cite>
+			<div> <p>Chariot rent-a-car / Todos los derechos reservados 2014 / Políticas de privacidad.</p></div>
+    	</footer>
+        </div>
+	</body>
+	</html>
