74372ec62b26a99e5ca6e254581039592e7aeb0e
aresavazquez <ares _vb>
2014-06-23 17:27:49 -0500

    redes sociales

--- app/views/contacto.blade.php
+++ app/views/contacto.blade.php
@@ -20,7 +20,7 @@
 <section class="icon-contacto">
 	<ul>
 		<li><div id="borde-blanco"><img src="../assets/whats.png">Whatsapp<span>55-3197-4069</span></div></li>
-		<li><div id="borde-blanco"><img src="../assets/facebook.png">Facebook<span>55-3197-4069</span></div></li>
+		<li><div id="borde-blanco"><img src="../assets/facebook.png"><link rel="stylesheet" type="text/css" href="http://www.facebook.com/chariotmx ">Facebook<span>55-3197-4069</span></div></li>
 		<li><div id="borde-blanco"><img src="../assets/celular.png">Celular<span>55-3197-4069</span></div></li>
 		<li><div id="borde-blanco"><img src="../assets/mercado-libre.png">Mercado Libre<span>55-3197-4069</span></div></li>
 	</ul>
