91ac28d1f71b037d02f5722eefd49a457be56d6a
Wenceslao Negrete <vampediem@gmail.com>
2014-06-26 12:27:35 -0500

    DB handle

--- app/views/categorias.blade.php
+++ app/views/categorias.blade.php
@@ -15,11 +15,11 @@
 </section>
 <section class="cuerpo">
 	<ul>
-		<li> <a href="{{ action('HomeController@detalle') }}"> <span>AUTOS CLASE A</span> </a><img src=""></li>
-		<li> <a href="{{ action('HomeController@detalle') }}"> <span>AUTOS CLASE B</span> </a><img src=""></li>
-		<li> <a href="{{ action('HomeController@detalle') }}"> <span>AUTOS CLASE C</span> </a><img src=""></li>
+		<li><a href="{{ action('HomeController@detalle') }}"> <span>AUTOS CLASE A</span> </a><img src=""></li>
+		<li><a href="{{ action('HomeController@detalle') }}"> <span>AUTOS CLASE B</span> </a><img src=""></li>
+		<li><a href="{{ action('HomeController@detalle') }}"> <span>AUTOS CLASE C</span> </a><img src=""></li>
 		@foreach ($categories as $category)
-			<li>{{ $category->id }}</li>
+			<li><a href="{{ action('HomeController@detalle') }}"> <span>{{ $category->name }}</span> </a><img src="{{ $category->img_path }}"></li>
 		@endforeach
 	</ul>
 </section>
