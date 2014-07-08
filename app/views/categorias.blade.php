c83024c49a70ccb45e85eff140e3d266a3070574
Wenceslao Negrete <vampediem@gmail.com>
2014-07-08 18:31:44 -0500

    category image

--- app/views/categorias.blade.php
+++ app/views/categorias.blade.php
@@ -9,7 +9,7 @@
 		<h1>{{ $title }}</h1>
 		<!--<h4>AUTO DEL MES <span>JETTA</span></h4>-->
 	</div>	
-	<div class="imagen">
+	<div class="imagen" style="background-image:url({{ asset($img) }})">
 		<!--img src=<"{{ asset($img) }}"-->
 	</div>
 </section>
