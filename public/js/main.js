31f2752560bac5e2dcb8bde705cc29ea265bf061
Wenceslao Negrete <vampediem@gmail.com>
2014-07-08 19:38:09 -0500

    behaviors

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,10 @@ $(document).on('ready', function(){
 	$('article.contenido').hide();
 	$('article.contenido.active').fadeIn('slow');
 	$('ul.informes li').on('click', function(e){
-		console.log(e.currentTarget);
+		var i = $(this).index();
+		$('article.contenido').removeClass('active');
+		$('article.contenido').hide();
+		$('article.contenido').eq(i).addClass('active');
+		$('article.contenido').eq(i).fadeIn();
 	});
 });
