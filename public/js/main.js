6c3b56f10991cb8617285a4d85f65783df83a5e7
Wenceslao Negrete <vampediem@gmail.com>
2014-07-08 19:43:23 -0500

    behaviors

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,16 @@
 $(document).on('ready', function(){
+	var $root = $('html, body');
+	if(window.location.hash){
+		 $root.animate({
+	        scrollTop: $( window.location.hash ).offset().top
+	    }, 500);
+	}
+	$('a').click(function() {
+	    $root.animate({
+	        scrollTop: $( $.attr(this, 'href') ).offset().top
+	    }, 500);
+	    return false;
+	})
 	$('article.contenido').hide();
 	$('article.contenido.active').fadeIn('slow');
 	$('ul.informes li').on('click', function(e){
